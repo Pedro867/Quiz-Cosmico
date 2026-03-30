e66a34d29d00d97aa2e57b1b1fe9745b2cd7ebc9
Pedro867 <pedrolucasmoreira123321@gmail.com>
2026-03-30 16:46:39 -0300

    adjust: padronização de código

--- js/qs.js
+++ js/qs.js
@@ -2,9 +2,9 @@ const questions = [
     {
         question: "Qual o nome da Galáxia que contém o Sistema Solar?",
         answers: [
-            { option: "Andrômeda", correct: false},
-            { option: "Via Láctea", correct: true},
-            { option: "Capricórnio", correct: false},
+            { option: "Andrômeda",       correct: false},
+            { option: "Via Láctea",      correct: true},
+            { option: "Capricórnio",     correct: false},
             { option: "Laticínios LTDA", correct: false},
         ],
     },
@@ -20,46 +20,46 @@ const questions = [
     {
         question: "Qual o Planeta mais Distante do Sol?",
         answers: [
-            { option: "Netuno", correct: true},
-            { option: "Plutão", correct: false},
-            { option: "Urano", correct: false},
+            { option: "Netuno",  correct: true},
+            { option: "Plutão",  correct: false},
+            { option: "Urano",   correct: false},
             { option: "Saturno", correct: false},
         ],
     },
     {
         question: "Qual o Planeta mais Próximo do Sol?",
         answers: [
-            { option: "Marte", correct: false},
+            { option: "Marte",    correct: false},
             { option: "Mercúrio", correct: true},
-            { option: "Vênus", correct: false},
-            { option: "Terra", correct: false},
+            { option: "Vênus",    correct: false},
+            { option: "Terra",    correct: false},
         ],
     },
     {
         question: "O que é um Buraco Negro?",
         answers: [
             { option: "Uma estrela recém formada", correct: false},
-            { option: "Uma estrela branca", correct: false},
-            { option: "Uma estrela colapsada", correct: true},
-            { option: "Lenda Urbana", correct: false},
+            { option: "Uma estrela branca",        correct: false},
+            { option: "Uma estrela colapsada",     correct: true},
+            { option: "Lenda Urbana",              correct: false},
         ],
     },
     {
         question: "O que é uma Nebulosa?",
         answers: [
-            { option: "Um grupo de estrelas", correct: false},
+            { option: "Um grupo de estrelas",      correct: false},
             { option: "Uma nuvem de gás e poeira", correct: true},
-            { option: "Uma estrela em explosão", correct: false},
-            { option: "Um tipo de Planeta", correct: false},
+            { option: "Uma estrela em explosão",   correct: false},
+            { option: "Um tipo de Planeta",        correct: false},
         ],
     },
     {
         question: "O que é uma estrela cadente?",
         answers: [
-            { option: "Planetas que se aproximam da Terra", correct: false},
-            { option: "Estrelas que caem do céu", correct: false},
+            { option: "Planetas que se aproximam da Terra",         correct: false},
+            { option: "Estrelas que caem do céu",                   correct: false},
             { option: "Meteoros que queimam na atmosfera da Terra", correct: true},
-            { option: "Estrelas distantes que morrem", correct: false},
+            { option: "Estrelas distantes que morrem",              correct: false},
         ],
     },
 ];
